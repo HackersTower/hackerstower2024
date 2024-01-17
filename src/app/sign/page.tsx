c4510aa2e11563306a673e3bdc66647e97dfbded
Sumit pal <tonystark83033@gmail.com>
2024-01-18 00:27:19 +0530

    feat:[Add nextAuth route]

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-<section className="py-16 px-8">
+<section className="py-16 px-8 ">
         <div className="text-center mb-10">
           <h2 className="text-3xl font-bold mb-3">SIGN UP</h2>
           <p className="text-gray-400">Join our community and start learning today!</p>
