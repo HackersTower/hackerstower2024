1f410355d2c02ab94e0305db166a74137269e99f
Sumit pal <tonystark83033@gmail.com>
2024-01-19 19:35:44 +0530

    feat:[Add state in sign input fiels]

--- src/app/sign/page.tsx
+++ src/app/sign/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 type Props = {}
 
 const page = (props: Props) => {
+  const [info,setInfo] = useState({fullName:"", email:"", password:"", cPassword:""});
+
+  const onChangeHandler = (e:any)=>{
+    setInfo({...info,[e.target.name]:[e.target.value]});
+    console.log(info);
+  }
+
   return (
 <section className="py-16 px-8 ">
         <div className="text-center mb-10">
@@ -11,10 +19,10 @@ const page = (props: Props) => {
           <p className="text-gray-400">Join our community and start learning today!</p>
         </div>
         <div className="flex flex-col items-center">
-          <Input className="mb-4 w-full md:w-1/2" placeholder="Full Name" />
-          <Input className="mb-4 w-full md:w-1/2" placeholder="Email" />
-          <Input className="mb-4 w-full md:w-1/2" placeholder="Password" type="password" />
-          <Input className="mb-4 w-full md:w-1/2" placeholder="Confirm Password" type="password" />
+          <Input className="mb-4 w-full md:w-1/2" placeholder="Full Name" name="fullName" onChange={onChangeHandler} />
+          <Input className="mb-4 w-full md:w-1/2" placeholder="Email" name="email"  onChange={onChangeHandler}/>
+          <Input className="mb-4 w-full md:w-1/2" placeholder="Password" type="password" name="password" onChange={onChangeHandler} />
+          <Input className="mb-4 w-full md:w-1/2" placeholder="Confirm Password" type="password" name="cPassword" onChange={onChangeHandler} />
           <Button className="bg-[#bd1e59] text-white w-full md:w-1/2">Sign Up</Button>
         </div>
       </section>
